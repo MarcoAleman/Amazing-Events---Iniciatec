4283be07a6c7b14f69f6a6dcfb06e69811cac212
kalanchoe18 <marcoaleman18@gmail.com>
2022-09-29 01:23:58 -0300

    Ambos filtros andando en el home

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 function qs(element){
     return document.querySelector(element);
 }
-
+//todo mensaje de error si el array esta vacio
 let printCards = (array, container)=>{
     container.innerHTML = '';
     array.forEach((event, i)=>{
@@ -44,95 +44,111 @@ let printDetail = (event, container)=>{
     container.appendChild(detail);
 }
 
-let searchFilter = (input, array, container)=> {
-    input.addEventListener('keyup', e => {
-        let querySearch = input.value.toLowerCase().trim();
-        let newArray = array.filter((item) => {
-            return item.name.toLowerCase().includes(querySearch)
-        })
-        printCards(newArray, container);
-    })
-}
-/* ------------------------------------------------------------------------------------------ */
-/* ------------------------------------------------------------------------------------------ */
-let allEvents = data.events;
-let currentDate = data.currentDate;
-let path = location.pathname;
-let cardContainer = qs('.collection-cards');
-let inputSearch = qs('.form-control');
-
-let pastEvents = allEvents.filter(e =>{
-    return e.date < currentDate;
-})
-
-let upEvents = allEvents.filter(e =>{
-    return e.date > currentDate;
-})
-
-//Implemetacion
-switch(true){
-    case path.includes('index'):
-        printCards(allEvents, cardContainer);
-        searchFilter(inputSearch, allEvents, cardContainer);
-        break;
-    case path.includes('past'):
-        printCards(pastEvents, cardContainer);
-        searchFilter(inputSearch, pastEvents, cardContainer);
-        break;
-    case path.includes('upcoming'):
-        printCards(upEvents, cardContainer);
-        searchFilter(inputSearch, upEvents, cardContainer);
-        break;
+const allCategories = (data) =>{
+    let categories = [];
+    data.forEach(event => categories.push(event.category))
+    categories = categories.filter((item, i) => categories.indexOf(item) === i)
+    return categories;
 }
 
-// Details
-let detalles = qs('#detail');
-let queryString = location.search;
-let params = new URLSearchParams(queryString);
-let id = params.get('id');
-
-if(detalles){
-    let eventDetail = allEvents.find(event => event._id == id);
-    printDetail(eventDetail, detalles)
-}
-
-/* ------------------------------------------------------------------------------------------ */
-/* Filtrar Caregorias */
-
-let categories = [];
-
-allEvents.forEach(element => {
-    categories.push(element.category);
-})
-
-categories = categories.filter((item, i) => {
-    return categories.indexOf(item) === i;
-})
-
-let categoryContainer = qs('.conteiner-checkbox');
-
-if(categoryContainer != null){
+const printCategories = (data, container) => {
+    let categories = allCategories(data);
     categories.forEach((category, i)=> {
         let checkbox = document.createElement('div');
         checkbox.className = "form-check form-check-inline"
         checkbox.innerHTML = `
         <input class="form-check-input" type="checkbox" id="cat${i}" value="${category}">
         <label class="form-check-label" for="cat${i}">${category}</label>`
-        categoryContainer.appendChild(checkbox);
+        container.appendChild(checkbox);
     })
 }
 
-let inputs = document.querySelectorAll('.form-check-input');
-console.log(inputs);
-/* ------------------------------------------------------------------------------------------ */
-/* Compruebo si los checkbox estan marcados*/
-/* let input = document.querySelectorAll('.form-check-input');
-console.log(input);
+const filterBySearch = (input, data) => {
+    let querySearch = input.value.toLowerCase().trim();
+    let result = data.filter((event) => {
+        return event.name.toLowerCase().includes(querySearch);
+    })
+    return result;
+}
 
-input.forEach(item => {
-    console.log(item.checked);
+const filterByCategory = (data) => {
+    let checkBoxes = document.querySelectorAll('.form-check-input');
+    let arrayChecks = Array.from(checkBoxes);
+    let checkOn = arrayChecks.filter( checkBox => checkBox.checked);
+    let categoryOn = checkOn.map(check => check.value);
+
+    if(categoryOn.length == 0){
+        return data;
+    }
+
+    let result = data.filter(event => categoryOn.includes(event.category));
+    return result;
+}
+
+const allFilters = () =>{
+    let resultByText = filterBySearch(inputSearch, allEvents);
+    let resultByCategory = filterByCategory(resultByText);
+    printCards(resultByCategory, cardContainer);
+}
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------ */
+let allEvents = data.events;
+let currentDate = data.currentDate;
+let pastEvents = allEvents.filter(e => e.date < currentDate);
+let upEvents = allEvents.filter(e => e.date > currentDate);
+let path = location.pathname;
+let cardContainer = qs('.collection-cards');
+let categoryContainer = qs('.conteiner-checkbox');
+let inputSearch = qs('.form-control');
+
+
+//Implemetacion
+switch(true){
+    case path.includes('index'):
+        //printCategories(allEvents, categoryContainer);
+        printCards(allEvents, cardContainer);
+        break;
+    case path.includes('past'):
+        //printCategories(allEvents, categoryContainer);
+        printCards(pastEvents, cardContainer);
+        break;
+    case path.includes('upcoming'):
+        //printCategories(allEvents, categoryContainer);
+        printCards(upEvents, cardContainer);
+        break;
+    case path.includes('contact'):
+        break;
+    case path.includes('detail'):
+}
+
+/* Filtrar Caregorias */
+if(categoryContainer != null){
+    printCategories(allEvents, categoryContainer);
+}
+let checks = document.querySelectorAll('.form-check-input');
+
+// Details
+let detailContainer = qs('#detail');
+let queryString = location.search;
+let params = new URLSearchParams(queryString);
+let id = params.get('id');
+
+if(detailContainer){
+    let eventDetail = allEvents.find(event => event._id == id);
+    printDetail(eventDetail, detailContainer)
+}
+
+/* ------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------- */
+//Agregando cosas
+inputSearch.addEventListener('keyup', e => {
+    allFilters()
 })
 
-input.addEventListener('input', e =>{
-
-}) */
+checks.forEach((item, i) => {
+    item.addEventListener('input', e => {
+        allFilters()
+    })
+})
+//Aca como podria evitarme el foreach
+//checks.addEventListener('input', e => {})
